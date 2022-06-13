f1f6eb33c8665f628b201ce80e1f38d39c7ec2bf
Gabriel <wootengabriel90@gmail.com>
2022-06-13 02:41:59 -0400

    removed superfluous comment

--- front-end/src/reservations/NewReservation.js
+++ front-end/src/reservations/NewReservation.js
@@ -3,15 +3,13 @@ import ErrorAlert from "../layout/ErrorAlert";
 import ReservationForm from "./ReservationForm";
 
 
-//submit button needs to be altered to show dashboard at the date of the new reservation
-
 function NewReservation({setDate}) {
   const [error, setError] = useState(null)
 
   return (
     <>
     <ErrorAlert error={error} />
-    <h3 className="">Create a reservation</h3>
+    <h3>Create a reservation</h3>
     <ReservationForm setDate={setDate} setError={setError} />
     </>
   );
