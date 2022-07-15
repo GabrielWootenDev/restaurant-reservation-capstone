703381a984c992397c7d4752c1dd12b2ee4d117b
Gabriel <wootengabriel90@gmail.com>
2022-07-14 23:55:12 -0400

    removed unused prop

--- front-end/src/tables/TableForm.js
+++ front-end/src/tables/TableForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function TableForm({ handleChange, handleCancel, handleSubmit, formData }) {
+function TableForm({ handleChange, handleCancel, handleSubmit }) {
   return (
     <>
       <div>
